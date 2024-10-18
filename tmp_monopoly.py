42c8f5c4460e5b6fec7a790e7e5d230b2a9cd7dd
CarlosMG06 <carlos.abmg06@gmail.com>
2024-10-18 16:30:13 +0200

    Aconseguit Mostrar L'Interfície (Taulell i InfoDreta) no manualment

--- tmp_monopoly.py
+++ tmp_monopoly.py
@@ -39,7 +39,7 @@ jugadors = {
 #--------------------------------------------------------------------------------------------------
 
 # Definicions -------------------------------------------------------------------------------------
-def sortida(jugador):
+def Sortida(jugador):
     jugadors[jugador]["diner"] += 200
 
 #--------------------------------------------------------------------------------------------------
@@ -67,7 +67,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 2
+            "ID_moviment": 2,
+            "fil_col_taulell": [6,5]
         },
         {
             "nom": "Rosselló",
@@ -80,7 +81,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 3
+            "ID_moviment": 3,
+            "fil_col_taulell": [6,4]
         },
         {
             "nom": "Marina",
@@ -93,7 +95,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 5
+            "ID_moviment": 5,
+            "fil_col_taulell": [6,2]
         },
         {
             "nom": "C. de cent",
@@ -106,7 +109,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 6
+            "ID_moviment": 6,
+            "fil_col_taulell": [6,1]
         },
         {
             "nom": "Muntaner",
@@ -119,7 +123,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 8
+            "ID_moviment": 8,
+            "fil_col_taulell": [5,0]
         },
         {
             "nom": "Aribau",
@@ -132,7 +137,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 9
+            "ID_moviment": 9,
+            "fil_col_taulell": [4,0]
         },
         {
             "nom": "Sant Joan",
@@ -145,7 +151,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 11
+            "ID_moviment": 11,
+            "fil_col_taulell": [2,0]
         },
         {
             "nom": "Aragó",
@@ -158,7 +165,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 12
+            "ID_moviment": 12,
+            "fil_col_taulell": [1,0]
         },
         {
             "nom": "Urquinaona",
@@ -171,7 +179,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 14
+            "ID_moviment": 14,
+            "fil_col_taulell": [0,1]
         },
         {
             "nom": "Fontana",
@@ -184,10 +193,11 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 15
+            "ID_moviment": 15,
+            "fil_col_taulell": [0,2]
         },
         {
-            "nom": "Les rambles",
+            "nom": "Les Rambles",
             "nom curt": "Rambles",
             "lloguer casa": 35,
             "lloguer hotel": 30,
@@ -197,7 +207,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 17
+            "ID_moviment": 17,
+            "fil_col_taulell": [0,4]
         },
         {
             "nom": "Pl. Catalunya",
@@ -210,7 +221,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 18
+            "ID_moviment": 18,
+            "fil_col_taulell": [0,5]
         },
         {
             "nom": "P. Àngel",
@@ -223,7 +235,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 20
+            "ID_moviment": 20,
+            "fil_col_taulell": [1,6]
         },
         {
             "nom": "Via Augusta",
@@ -236,7 +249,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 21
+            "ID_moviment": 21,
+            "fil_col_taulell": [2,6]
         },
         {
             "nom": "Balmes",
@@ -249,7 +263,8 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 23
+            "ID_moviment": 23,
+            "fil_col_taulell": [4,6]
         },
         {
             "nom": "Pg. de Gracia",
@@ -262,116 +277,204 @@ lista_caselles = {
             "nombre casas": 0,
             "nombre hotels": 0,
             "jugadors": [],
-            "ID": 24
+            "ID_moviment": 24,
+            "fil_col_taulell": [5,6]
         },
     ],
     "especials" : [
         {
             "nom": "Sortida",
             "nom curt":"Sortida",
-            "funcio": sortida,
+            "funcio": "Sortida",
             "jugadors": [],
-            "ID":1
+            "ID_moviment": 1,
+            "fil_col_taulell": [6,6]
         },
         {
             "nom": "Sort",
             "nom curt": "Sort",
-            "funcio": print(),
+            "funcio": "print()",
             "jugadors": [],
-            "ID": 4
+            "ID_moviment": 4,
+            "fil_col_taulell": [6,3]
         },
         {
             "nom": "Presó",
             "nom curt": "Presó", 
-            "funcio": print(),
+            "funcio": "print()",
             "jugadors": [],
-            "ID": 7
+            "ID_moviment": 7,
+            "fil_col_taulell": [6,0]
         },
         {
             "nom": "Caixa",
             "nom curt": "Caixa",
-            "funcio": print(),
+            "funcio": "print()",
             "jugadors": [],
-            "ID": 10
+            "ID_moviment": 10,
+            "fil_col_taulell": [3,0]
         },
         {
             "nom": "Parking",
             "nom curt": "Parking", 
-            "funcio": print(),
+            "funcio": "print()",
             "jugadors": [],
-            "ID": 13
+            "ID_moviment": 13,
+            "fil_col_taulell": [0,0]
         },
         {
             "nom":"Sort",
             "nom curt":"Sort",
-            "funcio": print(),
+            "funcio": "print()",
             "jugadors": [],
-            "ID": 16
+            "ID_moviment": 16,
+            "fil_col_taulell": [0,3]
         },
         {
             "nom":"Anar preso",
             "nom curt":"Anr pró",
-            "funcio": print(),
+            "funcio": "print()",
             "jugadors": [],
-            "ID": 19
+            "ID_moviment": 19,
+            "fil_col_taulell": [0,6]
         },
         {
             "nom":"Caixa",
             "nom curt":"Caixa",
-            "funcio": print(),
+            "funcio": "print()",
             "jugadors": [],
-            "ID": 22
+            "ID_moviment": 22,
+            "fil_col_taulell": [3,6]
         },
     ]
 }
 
-casillas_ordenados = sorted(lista_caselles["normals"]+lista_caselles["especials"], key=lambda casilla: casilla["ID"])
-for i1 in range(7):
-    for i2 in range(7):
-        print("""
-+--------+--------+--------+--------+--------+--------+--------+
-|        |        |        |        |        |        |        |
-|        |        |        |        |        |        |        |
-+--------+--------+--------+--------+--------+--------+--------+
-|        |                                            |        |
-|        |                                            |        |
-+--------+                                            +--------+
-|        |                                            |        |
-|        |                                            |        |
-+--------+                                            +--------+
-|        |                                            |        |
-|        |                                            |        |
-+--------+                                            +--------+
-|        |                                            |        |
-|        |                                            |        |
-+--------+                                            +--------+
-|        |                                            |        |
-|        |                                            |        |
-+--------+--------+--------+--------+--------+--------+--------+
-|        |        |        |        |        |        |        |
-|        |        |        |        |        |        |        |
-+--------+--------+--------+--------+--------+--------+--------+
-""", end="")
-#|{casilla["nom curt"].ljust(8)}|
+def MostrarCases(casella):
+    if casella.get("nombre cases",0) != 0:
+        return f"{casella["nombre cases"]}C"
+    else: return ""
+def MostrarHotels(casella):
+    if casella.get("nombre hotels",0) != 0:
+        return f"{casella["nombre hotels"]}H"
+    else: return ""
+def CasesIHotels(casella): 
+    return MostrarCases(casella)+MostrarHotels(casella)
+def LiniaNomsFilesCentrals(casella1,casella2):
+    liniaNoms = ""
+    
+    if MostrarCases(casella1) == "": 
+        liniaNoms += f"|{casella1["nom curt"].ljust(8)}|"
+    else: 
+        liniaNoms += f"|{casella2["nom curt"].ljust(7)}{MostrarCases(casella1)}"
+    
+    liniaNoms += "".ljust(44)
 
+    if MostrarCases(casella2) == "": 
+        liniaNoms += f"|{casella2["nom curt"].ljust(8)}|"
+    else: 
+        liniaNoms += f"{MostrarCases(casella2)}{casella2["nom curt"].ljust(7)}|"
+    
+    return liniaNoms
+def LiniaJugadorsFilesCentrals(casella1,casella2):
+    liniaJugadors = ""
+
+    jugadorsAbreujats = ""
+    for jugador in casella1["jugadors"]:
+        jugadorsAbreujats += jugador[0]
+    if MostrarHotels(casella1) == "": 
+        liniaJugadors += f"|{jugadorsAbreujats.ljust(8)}|"
+    else: 
+        liniaJugadors += f"{MostrarHotels(casella1)}{jugadorsAbreujats.ljust(7)}|"
+    
+    liniaJugadors += "".ljust(44)
+
+    jugadorsAbreujats = ""
+    for jugador in casella2["jugadors"]:
+        jugadorsAbreujats += jugador[0]
+    if MostrarHotels(casella2) == "": 
+        liniaJugadors += f"|{jugadorsAbreujats.ljust(8)}|"
+    else: 
+        liniaJugadors += f"{MostrarHotels(casella2)}{jugadorsAbreujats.ljust(7)}|"
+    
+    return liniaJugadors
+
+def Taulell():
+    lstCasellesOrdenades = sorted(lista_caselles["normals"]+lista_caselles["especials"], key=lambda casilla: casilla["fil_col_taulell"])
+    matriuCasellesOrdenades = [
+        lstCasellesOrdenades[:7],
+        lstCasellesOrdenades[7:9],
+        lstCasellesOrdenades[9:11],
+        lstCasellesOrdenades[11:13],
+        lstCasellesOrdenades[13:15],
+        lstCasellesOrdenades[15:17],
+        lstCasellesOrdenades[17:]
+    ]
+
+    lstStrTaulell = []
+    lstStrTaulell.append("--------".join("++++++++"))
+    for fila in range(7):
+        if fila == 0 or fila == 6:
+            lst = [""]
+            for columna in range(7):
+                lst.append(CasesIHotels(matriuCasellesOrdenades[fila][columna]).rjust(8,"-"))
+            lst.append("")
+            liniaVora = "+".join(lst)
+
+            lst = [""]
+            for columna in range(7):
+                lst.append(matriuCasellesOrdenades[fila][columna]["nom curt"].ljust(8))
+            lst.append("")
+            liniaNoms = "|".join(lst)
+
+            lst = [""]
+            for columna in range(7):
+                jugadorsAbreujats = ""
+                for jugador in matriuCasellesOrdenades[fila][columna]["jugadors"]:
+                    jugadorsAbreujats += jugador[0]
+                lst.append(jugadorsAbreujats.ljust(8))
+            lst.append("")
+            liniaJugadors = "|".join(lst)
+        else:
+            liniaVora = "+--------+"+"".ljust(44)+"+--------+"
+
+            casella1 = matriuCasellesOrdenades[fila][0]
+            casella2 = matriuCasellesOrdenades[fila][1]
+            liniaNoms = LiniaNomsFilesCentrals(casella1,casella2)
+            liniaJugadors = LiniaJugadorsFilesCentrals(casella1,casella2)
+
+        if fila == 6:
+            lstStrTaulell.append(liniaVora)
+            lstStrTaulell.append(liniaJugadors)
+            lstStrTaulell.append(liniaNoms)
+        else:
+            if 2<=fila<=5: lstStrTaulell.append(liniaVora)
+            lstStrTaulell.append(liniaNoms)
+            lstStrTaulell.append(liniaJugadors)
+            if fila == 0: lstStrTaulell.append(liniaVora)
+    lstStrTaulell.append("--------".join("++++++++"))
+
+    return lstStrTaulell
 
 #--------------------------------------------------------------------------------------------------
 
 
 # Info dreta --------------------------------------------------------------------------------------
-def infoDreta():
-    jugadores_ordenados = sorted(jugadors.items(), key=lambda jugador: jugador[1]["torn"])
+def InfoDreta():
+    lstStrInfoDreta = []
+    jugadorsOrdenats = sorted(jugadors.items(), key=lambda jugador: jugador[1]["torn"])
 
-    for jugador, info in jugadores_ordenados:
+    for jugador, info in jugadorsOrdenats:
         if jugador == "Banca":
-            print(f"""{jugador}:
-    Diners: {info['diner']}€
-        """)
+            lstStrInfoDreta.extend([f"{jugador}:", f"Diners: {info['diner']}€",""])
         else:
-            print(f"""Jugador {jugador}:
-    Carrers: {info['carrers']}
-    Diners: {info['diner']}€
-    Especial: {info['especial']}
-            """)
+            lstStrInfoDreta.extend([f"Jugador {jugador}:", f"Carrers: {info['carrers']}", f"Diners: {info['diner']}€", f"Especial: {info['especial']}",""])
+    return lstStrInfoDreta
 #--------------------------------------------------------------------------------------------------
 
+def MostrarInterficie():
+    taulell = Taulell()
+    infoDreta = InfoDreta()
+    for liniaTaulell, liniaDreta in list(zip(taulell,infoDreta)):
+        print(liniaTaulell+"   "+liniaDreta)
+
+MostrarInterficie()
