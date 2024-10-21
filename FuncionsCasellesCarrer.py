4b546c81c65bb868648aefcbda41d8e348d9aa23
CarlosMG06 <carlos.abmg06@gmail.com>
2024-10-21 15:56:38 +0200

    Fet Funció VendreAlBanc( )

--- FuncionsCasellesCarrer.py
+++ FuncionsCasellesCarrer.py
@@ -4,18 +4,23 @@ from Interficie import MostrarInterficie
 #--------------------------------------------------------------------------------------------------
 
 def ComprarTerreny(IDCasella, nomJugador):
-    preu = caselles[IDCasella]["comprar terreny"]
+    c = caselles[IDCasella]
+    preu = c["comprar terreny"]
     caselles[IDCasella]["propietari"] = nomJugador
+    jugadors[nomJugador]["carrers"] += c["nom"]
     jugadors[nomJugador]["diners"] -= preu
     altresDades["diners banca"] += preu
+    AfegirAHistorial(f"  \"{nomJugador[0]}\" compra el terreny")
 def ComprarCasa(IDCasella, nomJugador):
     preu = caselles[IDCasella]["comprar casa"]
     jugadors[nomJugador]["diners"] -= preu
     altresDades["diners banca"] += preu
+    AfegirAHistorial(f"  \"{nomJugador[0]}\" compra 1 casa")
 def ComprarHotel(IDCasella, nomJugador):
     preu = caselles[IDCasella]["comprar hotel"]
     jugadors[nomJugador]["diners"] -= preu
     altresDades["diners banca"] += preu
+    AfegirAHistorial(f"  \"{nomJugador[0]}\" compra 1 hotel")
 
 def Preus(IDCasella):
     pass
@@ -33,8 +38,30 @@ def PreuJugador(nomJugador):
     MostrarInterficie()
 
 def VendreAlBanc(IDCasella, nomJugador):
+    c = caselles[IDCasella]
+    pagament = c["lloguer casa"] * c["nombre cases"] + c["lloguer hotel"] * c["nombre hotels"]
+
+    for carrer in jugadors[nomJugador]["carrers"]:
+        for casella in caselles:
+            if casella["nom"] == carrer:
+                cCarrer = caselles[caselles.index(casella)]
+                caselles[caselles.index(casella)]["propietari"] = "Banca"
+                caselles[caselles.index(casella)]["nombre cases"] = 0
+                caselles[caselles.index(casella)]["nombre hotels"] = 0
+                preu = (cCarrer["comprar terreny"] + c["comprar casa"] * c["nombre cases"] + c["comprar hotel"] * c["nombre hotels"]) * 0.5
+                altresDades["diners banca"] -= preu
+                jugadors[nomJugador]["diners"] += preu
+                break
+    AfegirAHistorial(f"  \"{nomJugador[0]}\" ven les seves propietats al banc")
+
+    jugadors[nomJugador]["diners"] -= pagament
+    jugadors[c["propietari"]]["diners"] += pagament
+    AfegirAHistorial(f"  \"{nomJugador[0]}\" paga {pagament}€ a \"{c["propietari"][0]}\"")
+    
     pass
-def VendreAJugador(IDCasellaDesti,nomJugadorVenedor,nomJugadorComprador):
+def VendreAJugador(IDCasella,nomJugadorVenedor,nomJugadorComprador):
+    c = caselles[IDCasella]
+    pagament = c["lloguer casa"] * c["nombre cases"] + c["lloguer hotel"] * c["nombre hotels"]
     pass
 
 def CaureEnCasellaCarrer(IDCasella, nomJugador):
